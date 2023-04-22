0df3e9d91474bae4c5ead7b06753d56af20e23ab
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-04-22 16:16:42 -0300

    Adicionando script,estilizancao e novas marcacoes

--- script.js
+++ script.js
@@ -1,5 +1,61 @@
-var botao = document.querySelector(".botao");
+var botoes = document.querySelectorAll(".botao");
+var tema = document.querySelector(".modo-visual");
+var principal = document.querySelector(".principal");
+var imgTema = document.querySelector(".imagem__trocar--fundo");
+var modoTema = document.querySelector("modo-visual");
+var modoAtual = "claro";
 
-botao.addEventListener("click",()=>{
-    
+ botoes.forEach((botao)=>{
+    botao.addEventListener("click",(evento)=>{
+        var elemento = evento.target;
+        console.log(elemento);
+        var textoElemento = elemento.textContent;
+        descobrirRede(textoElemento)
+    })
 })
+
+tema.addEventListener("click",()=>{
+    principal.classList.toggle("principal--escuro");
+    botoes.forEach(botao=>{
+        botao.classList.toggle("botao--escuro");
+    })
+    trocarModo();
+
+})
+
+function descobrirRede(texto){
+    var i = 0;
+    var redesSociais = ["GitHub","Linkedin","Instagram","Twitter"];
+            if(texto == redesSociais[i]){
+                window.location.href = "https://github.com/Ital023";
+            }
+            if(texto == redesSociais[i+1]){
+               
+                window.location.href = "https://www.linkedin.com/in/italomirandafont/";
+            }
+            if(texto == redesSociais[i+2]){
+                
+                window.location.href = "https://www.instagram.com/italo_m20/";
+            }
+            if(texto == redesSociais[i+3]){
+                window.location.href = "https://twitter.com/italo_m20";
+            }
+}
+
+function verificarModo() {
+    if (modoAtual === "claro") {
+      modoAtual = "escuro";
+    } else {
+      modoAtual = "claro";
+    }
+  }
+  function trocarModo(){
+    verificarModo();
+    if(modoAtual === "claro"){
+        imgTema.src = "./assets/img/lua.png"
+        imgTema.classList.remove("nova-imagem--escura")
+    }else{
+        imgTema.src = "./assets/img/sun-solid.svg";
+        imgTema.classList.add("nova-imagem--escura");
+    }
+  }
